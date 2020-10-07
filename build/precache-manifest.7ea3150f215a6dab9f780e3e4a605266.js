29365770aeffa47ea2cd9c280857e88a484c8e6b
Faris Adlin <shifair@192.168.1.2>
2020-10-07 17:24:29 +0700

    First all

---
+++ build/precache-manifest.7ea3150f215a6dab9f780e3e4a605266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08bc2c038485db98a42",
+    "url": "/static/css/main.617533a4.chunk.css"
+  },
+  {
+    "revision": "b08bc2c038485db98a42",
+    "url": "/static/js/main.b08bc2c0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9a6fbbfb35d790069cad",
+    "url": "/static/js/2.9a6fbbfb.chunk.js"
+  },
+  {
+    "revision": "c72292af4b2bd26ddab360d0f7808e11",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/timeline.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      <h2>IT Associate Consultant (PT. Gamatechno Indonesia) <span><br />December 2019-June 2020</span></h2>
+                        <p>To be an IT associate consultant for our B2B's client, including local government and corporations, to solve their problems with our IT solution.</p>
+                        <p>My Achievements:</p>
+                        <ul>
+                          <li>Assisted the production team in making the master plan and blueprint.</li>
+                          <li>Helped the survey team gather data related to the requirements of the master plan in the Regional Government.</li>
+                          <li>Held presentations for clients to make the project easily understandable so they can work together much more comfortable with us.</li>
+                          <li>Assisted the marketing team in preparing the Budget Plan or Rencana Anggaran Belanja (RAB) and also the Terms of Reference or Kerangka Acuan Kerja (KAK) for clients.</li>
+                          <li>Supported the marketing team in using tools for Email and Whatsapp Marketing.</li>
+                          <li>Helped the division as online marketing when organizing Gamatechno's Online Sharing Session event.</li>
+                        </ul>
+                        <p>Some of my projects including:</p>
+                        <ul>
+                          <li>2020 e-GIF (e-Government Interoperability Framework) Trenggalek Regency.</li>
+                          <li>2020 e-GIF (e-Government Interoperability Framework) Banjar Regency.</li>
+                          <li>2020 SOP (Standard Operational Procedure) ICT (Information and Communication Technology) Banjar Regency.</li>
+                          <li>2020 e-GIF (e-Government Interoperability Framework) Bulungan Regency.</li>
+                          <li>2020 Masterplan e-Government or SPBE (Sistem Pemerintahan Berbasis Elektronik) Tegal Regency.</li>
+                          <li>2019 Masterplan ICT (Information and Communication Technology) Pertamina Patra Niaga.</li>
+                          <li>2019 SOP (Standard Operational Procedure) ICT (Information and Communication Technology) Pertamina Patra Niaga.</li>
+                          <li>2019 DED (Detail Engineering Design) Backbone Batu Bara Regency.</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Social Media Analyst (Kamtiv.id) <span><br />November 2018-December 2019</span></h2>
+                        <p>My Job Description are:</p>
+                        <ul>
+                          <li>Gathered social media data according to the client's social media using 3rd party's applications such as Social Sprout.</li>
+                          <li>Analyzed insight data gave by social media using a software tool such as Microsoft Power BI.</li>
+                          <li>To analyze insight data given by social media and provide the business client with presentable and easy to understand data. So the client will get benefits. Such as:</li>
+                            <ul>
+                              <li>What type of content they needed to post.</li>
+                              <li>What is the best time to post the draft.</li>
+                              <li>Which hashtags that will make the best interaction with customers.</li>
+                            </ul>
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Master of Engineering (M. Eng), Information Technology Engineering, Universitas Gadjah Mada <span><br />January 2017-October 2019</span></h2>
+                        <p>I have completed my master of engineering with major in Information Technology engineering, Universitas Gadjah Mada with 3.54 GPA</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Bachelor of Engineering (B. Eng), Electrical Engineering, Universitas Gadjah Mada <span><br />August 2009-August 2016</span></h2>
+                        <p>I have completed my bachelor of engineering with major in Electrical Engineering, Universitas Gadjah Mada with 2.56 GPA</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
